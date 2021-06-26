c092e21672a0a3e80ce2d4581c7ea4504e79bec4
Lucas <lucasabreusilva.98@gmail.com>
2021-06-26 13:42:55 -0300

    Tentatida de script para rodar cadastro do user automatico

--- cinguloapi/activities/api/outro_teste_qualquer.py
+++ cinguloapi/activities/api/outro_teste_qualquer.py
@@ -6,11 +6,11 @@ url = "https://static.cingulo.com/bi/user_activities.json"
 response_data = requests.get(url)
 data_users = response_data.json()
 
+user_activity = UserActivity()
 
-i = 0
-while i < 40:
-    user_activity = UserActivity()
+
+for i in range(40):
     user_activity.user_id = data_users[i]['id']
     user_activity.activities = data_users[i]['activities']
     user_activity.save()
-    i += 1
+    user_activity = UserActivity()
--- cinguloapi/urls.py
+++ cinguloapi/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from cinguloapi.activities.api.viewsets import UserActivityViewSet, JanuaryDailyActivityViewSet, MonthDailyActivityViewSet
+from cinguloapi.activities.api.viewsets import UserActivityViewSet, JanuaryDailyActivityViewSet,\
+    MonthDailyActivityViewSet
 
 router = routers.DefaultRouter()
 router.register(r'useractivities', UserActivityViewSet)
